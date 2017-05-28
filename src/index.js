c00c06ea519e6232ddc101c00298464fdc832040
kjendrzyca <kjendrzyca@gmail.com>
2017-05-28 18:20:05 +0200

    added ui-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {UIRouter, UIView, UISref, UISrefActive, pushStateLocationPlugin} from 'ui-router-react'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+var helloState = {
+  name: 'hello',
+  url: '/hello',
+  component: () => <App />
+}
+
+var aboutState = {
+  name: 'about',
+  url: '/about',
+  component: () => <h3>Its the UI-Router hello world app!</h3>
+}
+
+ReactDOM.render(
+  <UIRouter plugins={[pushStateLocationPlugin]} states={[helloState, aboutState]}>
+    <div>
+      <UISrefActive class="active">
+        <UISref to="hello"><a>App</a></UISref>
+      </UISrefActive>
+      <UISrefActive class="active">
+        <UISref to="about"><a>About</a></UISref>
+      </UISrefActive>
+
+      <UIView/>
+    </div>
+  </UIRouter>,
+  document.getElementById('root')
+)
