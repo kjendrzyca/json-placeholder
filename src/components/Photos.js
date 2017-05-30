b4b82398b5b146866b00c71da74555216d8c2f68
kjendrzyca <kjendrzyca@gmail.com>
2017-05-30 19:41:03 +0200

    added modal for full size photo

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {Col, Grid, Row, Thumbnail} from 'react-bootstrap'
+import {Button, Col, Image, Modal, Row, Thumbnail} from 'react-bootstrap'
+import './Photos.css'
 
 class Photos extends Component {
+  state = {
+    selectedPhotoId: 0
+  }
+
+  showFullSizeImage = (selectedPhotoId) => {
+    this.setState({selectedPhotoId: selectedPhotoId})
+  }
+
+  closeFullSizeImage = () => {
+    this.setState({selectedPhotoId: 0})
+  }
+
   render() {
     const {photos} = this.props.resolves
+    const selectedPhoto = photos.find(photo => photo.id === this.state.selectedPhotoId)
 
     return (
       <Row>
         {photos.map(photo => (
           <Col key={photo.id} xs={6} md={2}>
-            <Thumbnail src={photo.thumbnailUrl} />
+            <Thumbnail
+              onClick={() => this.showFullSizeImage(photo.id)}
+              src={photo.thumbnailUrl}
+            />
           </Col>
         ))}
+
+        {selectedPhoto && <Modal show={true} onHide={this.closeFullSizeImage}>
+          <Modal.Header closeButton>
+            <Modal.Title>{selectedPhoto.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Image src={selectedPhoto.url} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeFullSizeImage}>Close</Button>
+          </Modal.Footer>
+        </Modal>}
       </Row>
     )
   }
